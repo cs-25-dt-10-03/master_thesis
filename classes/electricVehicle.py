9769ecbff5f665d42b0627a706b5a5ed869bdcde
Smedten <miksmedt@gmail.com>
2025-02-13 11:39:38 +0100

    added function to convert vehicle to DFO

--- classes/electricVehicle.py
+++ classes/electricVehicle.py
@@ -1,6 +1,8 @@
 from datetime import datetime, timedelta
 from typing import Optional
 from classes.flexOffer import flexOffer
+from config import config
+from classes.DFO import DependencyPolygon, DFO
 
 class ElectricVehicle:
     def __init__(self, vehicle_id: str, 
@@ -20,26 +22,16 @@ class ElectricVehicle:
         self.current_soc = initial_soc if initial_soc is not None else soc_min
 
 
-
-
     def create_flex_offer(self,
                           charging_window_start: datetime,
                           charging_window_end: datetime,
-                          time_slot_resolution: timedelta,
-                          required_duration: timedelta) -> flexOffer:
+                          duration: timedelta) -> flexOffer:
 
-        valid_resolutions = {timedelta(hours=1), timedelta(minutes=15)}
+        time_slot_resolution = timedelta(minutes = config.TIME_RESOLUTION)
 
-        if time_slot_resolution not in valid_resolutions:
-            raise ValueError("Time slot resolution must be either 1 hour or 15 minutes.")
-        
-        if charging_window_end - charging_window_start < required_duration:
-            raise ValueError("Charging window is too short for the required charging duration.")
-        
-        latest_start = charging_window_end - required_duration
+        latest_start = charging_window_end - duration
 
-        flex_duration = required_duration
-        num_slots = int(flex_duration / time_slot_resolution)
+        num_slots = int(duration / time_slot_resolution)
         energy_per_slot = self.charging_power * (time_slot_resolution.total_seconds() / 3600)
 
         energy_profile = [energy_per_slot * self.charging_efficiency for _ in range(num_slots)]
@@ -51,13 +43,41 @@ class ElectricVehicle:
             offer_id=self.vehicle_id,
             earliest_start=charging_window_start,
             latest_start=latest_start,
-            duration=flex_duration,
+            duration=duration ,
             energy_profile=energy_profile,
             min_energy=min_energy,
             max_energy=max_energy,
             total_energy_limit=self.capacity
         )
         return flex_offer
+    
+    def create_dfo(self, numsamples, duration) -> DFO:
+
+        time_slot_resolution = timedelta(minutes = config.TIME_RESOLUTION)     
+
+        num_slots = int(duration / time_slot_resolution)
+
+        initial_energy = self.current_soc * self.capacity
+        target_min_energy = self.soc_min * self.capacity
+        target_max_energy = self.soc_max * self.capacity
+
+        additional_min = max(target_min_energy - initial_energy, 0)
+        additional_max = max(target_max_energy - initial_energy, 0)
+
+        min_prev = []
+        max_prev = []
+
+        for i in range(num_slots):
+            min_prev[i] = min((additional_min / self.charging_power) * (i + 1), additional_min)
+            max_prev[i] = min((additional_max / self.charging_power) * (i + 1), additional_max)
+
+        dfo = DFO(self.vehicle_id, min_prev, max_prev, numsamples)
+        dfo.generate_dependency_polygons()
+        return dfo
+
+    def update_soc(self, charged_energy):
+        new_energy = self.capacity * self.current_soc + charged_energy
+        self.current_soc = new_energy / self.capacity
 
     def __repr__(self):
         return (f"<EV {self.vehicle_id}: SoC={self.current_soc*100:.0f}% "
