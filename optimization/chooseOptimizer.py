e4a06366281623920fef708dc00a1ef744841c40
Smedten <miksmedt@gmail.com>
2025-02-20 10:58:48 +0100

    Removed optimization

--- optimization/chooseOptimizer.py
+++
@@ -1,7 +0,0 @@
-from optimization.multiEvOptimizer import MultiEvOptimizer
-
-def optimizer_factory(optimizer_type: str, **kwargs):
-    if optimizer_type == 'multi_ev':
-        return MultiEvOptimizer(**kwargs)
-    else:
-        raise ValueError(f"Unknown optimizer type: {optimizer_type}")
