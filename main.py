27ea241d4c6d91aefdbe3ec981de3fefb22afc84
Raymond2479 <rayersej@gmail.com>
2025-03-03 11:35:20 +0100

    Example main

--- main.py
+++ main.py
@@ -1,13 +1,77 @@
+from datetime import datetime, timedelta
+from classes.electricVehicle import ElectricVehicle
+from config import config
+from classes.DFO import DFO, DependencyPolygon
+from aggregation.DFO_aggregation import agg2to1
+from disaggregation.DFO_disaggregation import disagg1to2, disagg1toN
 from database.dataManager import fetch_all_offers
 
 def main():
-    result = fetch_all_offers()
+    tesla_model_y = ElectricVehicle(
+        vehicle_id=1,
+        capacity=75.0,
+        soc_min=0.20,
+        soc_max=0.80,
+        charging_power=7.0,
+        charging_efficiency=0.84,
+        #initial_soc=0.30
+    )
 
-    for sfo in result["sfo"]:
-        print(sfo)
+    tesla_model_s = ElectricVehicle(
+        vehicle_id=2,
+        capacity=100.0,
+        soc_min=0.40,
+        soc_max=0.80,
+        charging_power=10.0,
+        charging_efficiency=0.84,
+        #initial_soc=0.30
+    )
 
-    for tec in result["tec"]:
-        print(tec)
+    tesla_model_s = ElectricVehicle(
+        vehicle_id=2,
+        capacity=100.0,
+        soc_min=0.40,
+        soc_max=0.80,
+        charging_power=10.0,
+        charging_efficiency=0.84,
+        #initial_soc=0.30
+    )
+
+    charging_window_start = datetime.now().replace(hour=22, minute=0, second=0, microsecond=0)
+    charging_window_end = charging_window_start + timedelta(hours=8)
+    duration = timedelta(hours=3) 
+
+    dfo1 = tesla_model_y.create_dfo(charging_window_start,
+                                   charging_window_end,
+                                   duration,
+                                   numsamples=4)
+
+    dfo2 = tesla_model_s.create_dfo(charging_window_start+timedelta(hours=1),
+                                   charging_window_end,
+                                   duration,
+                                   numsamples=4)
+
+    print("Generated DFO for", tesla_model_y.vehicle_id)
+    #dfo.print_dfo()
+    #print(dfo)
+    dfo1.plot_dfo()
+    dfo2.plot_dfo()
+    dfo3 = agg2to1(dfo1, dfo2, 4)
+    dfo3.plot_dfo()
+    # Create sample DFOs
+
+    # Define a reference schedule
+    #yA_ref = [4.0, 8.0, 10.0]
+
+    # Disaggregate into two
+    #y1_ref, y2_ref = disagg1to2(dfo1, dfo2, dfo3, yA_ref)
+    #print("Disaggregated y1_ref:", y1_ref)
+    #print("Disaggregated y2_ref:", y2_ref)
+
+    # Disaggregate into multiple DFOs
+    #dfos = [dfo1, dfo2]  # Assume these are the original DFOs
+    #y_refs = disagg1toN(dfo3, dfos, yA_ref)
+    #print("Disaggregated y_refs:", y_refs)
     
 
 if __name__ == "__main__":
