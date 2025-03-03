de3626d9458b3e927e00aa42b2be6cf57a6c79d9
Smedten <miksmedt@gmail.com>
2025-03-03 13:21:00 +0100

    added tests

--- optimization/flexOfferOptimizer.py
+++ optimization/flexOfferOptimizer.py
@@ -1,20 +1,20 @@
 from pulp import LpProblem, LpMinimize, LpVariable, lpSum
 from datetime import timedelta
+from typing import List
+from classes.flexOffer import flexOffer
+from database.SpotPriceData import SpotPriceData
 import pandas as pd
 from config import config
 
 class FlexOfferOptimizer:
-    def __init__(self, flex_offers, spot_price_data, time_resolution=config.TIME_RESOLUTION):
+    def __init__(self, flex_offers, time_resolution=config.TIME_RESOLUTION):
         self.flex_offers = flex_offers
-        self.spot_price_data = spot_price_data
+        self.spot_price_data = SpotPriceData('database/spotPrices.csv')
         self.time_resolution = timedelta(minutes=time_resolution)
 
-    def optimize(self):
+    def optimize(self) -> List[flexOffer]:
         prob = LpProblem("FlexOfferScheduling", LpMinimize)
 
-        # Decision variables:
-        # - For each offer, a binary variable for each possible start time.
-        # - For each offer and each possible start time, a continuous energy variable for each time slot.
         start_vars = {}
         energy_vars = {}
 
@@ -26,7 +26,7 @@ class FlexOfferOptimizer:
             for t in start_times:
                 energy_vars[offer][t] = []
                 for i, (min_e, max_e) in enumerate(offer.energy_profile):
-                    var = LpVariable(f"Energy_{offer.offer_id}_{t.strftime('%Y%m%d%H%M')}_{i}", lowBound=0, cat='Continuous')
+                    var = LpVariable(f"Energy_{offer.offer_id}_{t.strftime('%Y%m%d%H%M')}_{i}", lowBound=min_e, cat='Continuous')
                     energy_vars[offer][t].append(var)
 
         # Objective: minimize total cost across all offers.
@@ -34,7 +34,6 @@ class FlexOfferOptimizer:
         for offer in self.flex_offers:
             for t in start_vars[offer]:
                 for i, (min_e, max_e) in enumerate(offer.energy_profile):
-                    # The spot price is taken at the time slot corresponding to the start time + offset.
                     price = self.spot_price_data.get_price_by_datetime(t + i * self.time_resolution)
                     total_cost.append(price * energy_vars[offer][t][i])
         prob += lpSum(total_cost)
@@ -47,8 +46,6 @@ class FlexOfferOptimizer:
             # (2) For each potential start time and slot, link the energy variable with the start decision.
             for t in start_vars[offer]:
                 for i, (min_e, max_e) in enumerate(offer.energy_profile):
-                    # If start time t is not chosen (x = 0), then the energy must be 0.
-                    # If chosen (x = 1), energy must lie between min_e and max_e.
                     prob += energy_vars[offer][t][i] <= max_e * start_vars[offer][t], f"MaxEnergy_{offer.offer_id}_{t}_{i}"
                     prob += energy_vars[offer][t][i] >= min_e * start_vars[offer][t], f"MinEnergy_{offer.offer_id}_{t}_{i}"
 
